b4e75020f9b9dd3afaaf72a2edcdc8ccfff0964b
Riu <sleepygirl2023@gmail.com>
2023-10-26 22:00:19 +0800

    add whale script into package.json

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 # Update `config.js` with your own parameters first!
 # Then run `sh test.sh`
 
-npm run whale -- 0x39a5d1a636bade0ce8db69fa5aebfbe203c3ead2
+npm run whale
 npm run compile-local && npm run exec-local -- 18370576
